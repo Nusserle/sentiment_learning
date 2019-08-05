47a9ff2ece7b4d015eb5f9cda23b5eec4dc98576
Nusserle <tobiasnusser@web.de>
2019-08-05 16:16:16 +0200

    Added conceptnet placeholder

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 import tensorflow as tf
@@ -5,6 +6,8 @@ from tensorflow import keras
 from keras import models
 from keras import layers
 from keras import optimizers
+from keras import losses
+from keras import metrics
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
@@ -47,6 +50,36 @@ def clean_regex(tweet):
     cleaned3 = re.sub('www.*?(?:\s|$)', '', cleaned2)
     return cleaned3
 
+def parse_to_dict(file_path):
+    """
+    Creates hashmap with word as key and concept vector as value
+    :param file_path: path to the conceptnet dictionary file
+    :return: hashmap of word and vectors
+    """
+    concept_hash = {}
+    with open(file_path, encoding="utf8") as f:
+        text = f.readlines()[1:]
+        for line in text:
+            first_item = line.split(" ").__getitem__(0)
+            concept_hash[first_item] = line
+    f.close()
+    return concept_hash
+
+def find_word(word, concept_hash):
+    """
+    Finds conceptnet vector for a word in the conceptnet hashmap
+    :param word: input word to analyze
+    :param concept_hash: hashmap of word and conceptnet vector
+    :return: returns the appropriate vector or none if its not in the hashmap
+    """
+    if word in concept_hash.keys():
+        vector = concept_hash[word].split(" ")[1:]
+        vector = [float(i) for i in vector]
+    else:
+        vector = None
+    return vector
+
+
 # 160.000 entries
 training_data, training_label = preprocess(read_in("data/training.csv"))
 test_data, test_label = preprocess(read_in("data/test.csv"))
@@ -56,32 +89,36 @@ x_val = training_data[140000:]
 train_label = np.asarray(training_label[:140000])
 val_label = np.asarray(training_label[140000:])
 
+# print("pre")
+sents = [["He", "likes", "tea"], ["She", "does", "n't", "like", "tea"]]
+# e = Embedder('C:/Users/Tobias.Nusser/PycharmProjects/sentiment_learning/elmo_embedding/144/')
+# embedding = e.sents2elmo(sents)
+concept_hash = parse_to_dict(
+            os.path.join(os.path.dirname(__file__)) + "/conceptnet/numberbatch-en.txt")
+for sent in sents:
+    for word in sent:
+        print(word, find_word(word, concept_hash))
 
-#print("pre")
-sents = [["He", "likes", "tea"], ["She", "does" ,"n't", "like" ,"tea"]]
-e = Embedder('C:/Users/Tobias.Nusser/PycharmProjects/sentiment_learning/elmo_embedding/144/')
-embedding = e.sents2elmo(sents)
-
-model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
-model.compile(optimizer=optimizers.RMSprop(lr=0.001),
-loss=losses.binary_crossentropy,
-metrics=[metrics.binary_accuracy])
-model.compile(optimizer='rmsprop',
-loss='binary_crossentropy',
-metrics=['acc'])
-history = model.fit(partial_x_train,
-partial_y_train,
-epochs=20,
-batch_size=512,
-validation_data=(x_val, y_val))
-
+# model = models.Sequential()
+# model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+# model.add(layers.Dense(16, activation='relu'))
+# model.add(layers.Dense(1, activation='sigmoid'))
+# model.compile(optimizer=optimizers.RMSprop(lr=0.001),
+#               loss=losses.binary_crossentropy,
+#               metrics=[metrics.binary_accuracy])
+# model.compile(optimizer='rmsprop',
+#               loss='binary_crossentropy',
+#               metrics=['acc'])
+# history = model.fit(partial_x_train,
+#                     partial_y_train,
+#                     epochs=20,
+#                     batch_size=512,
+#                     validation_data=(x_val, y_val))
+#
 history_dict = history.history
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
-epochs = range(1, len(acc) + 1)
+epochs = range(1, len(loss_values) + 1)
 plt.plot(epochs, loss_values, 'bo', label='Training loss')
 plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
 plt.title('Training and validation loss')
